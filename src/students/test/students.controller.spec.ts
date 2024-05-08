57ac715c36767d29852cb5709a9e1bd3c1aea803
RoomeyRahman <roomeyrahman@gmail.com>
2024-05-08 21:28:49 +0600

    add findAll unit test for controller

--- src/students/test/students.controller.spec.ts
+++ src/students/test/students.controller.spec.ts
@@ -1,20 +1,62 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StudentsController } from '../controllers/students.controller';
 import { StudentsService } from '../services';
+import {
+  mockStudent,
+  mockUser,
+  mockCreateStudentDto,
+  mockStudents,
+  mockQuery,
+} from '../../common/mock/test.mock';
+import { SearchStudentDto } from '../dto';
 
 describe('StudentsController', () => {
   let controller: StudentsController;
+  let service: StudentsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [StudentsController],
-      providers: [StudentsService],
+      providers: [
+        {
+          provide: StudentsService,
+          useValue: {
+            create: jest.fn(),
+            update: jest.fn(),
+            findAll: jest.fn(),
+            findOne: jest.fn(),
+            count: jest.fn(),
+            delete: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<StudentsController>(StudentsController);
+    service = module.get<StudentsService>(StudentsService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create a new student', async () => {
+      jest.spyOn(service, 'create').mockResolvedValue(mockStudent);
+      const result = await controller.create(mockCreateStudentDto, mockUser);
+      expect(result).toEqual(mockStudent);
+      expect(service.create).toHaveBeenCalled();
+    });
+  });
+
+  describe('findAll', () => {
+    it('should find all students', async () => {
+      const mockResult = {
+        data: mockStudents,
+      };
+      jest.spyOn(service, 'findAll').mockResolvedValue(mockResult);
+      const result = await controller.findAll(mockQuery as SearchStudentDto);
+      expect(service.findAll).toHaveBeenCalled();
+    });
   });
 });
